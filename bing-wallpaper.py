0c0162a71f1cda0ad982bfef8741d310d0b72e0f
alsotang <alsotang@gmail.com>
2021-07-07 12:00:41 +0800

    use python script instead of shell

---
+++ bing-wallpaper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import urllib.request
+import re
+import os
+
+root_dir = os.path.dirname(__file__)
+
+bing_index_url = 'https://cn.bing.com/'
+
+def main():
+    page_src = urllib.request.urlopen(bing_index_url).read()
+
+    href_m = re.search(r"""<link id="bgLink".+?href="(.+?)".+?>""", str(page_src))
+    if href_m is None:
+        return
+
+    href = href_m.group(1)
+    print('href', href)
+
+    image_name_m = re.search(r"/th\?id=(.+?)&", href)
+    image_name = image_name_m.group(1)
+    print('image_name', image_name)
+
+    image_url = '{}{}'.format(bing_index_url, href)
+    urllib.request.urlretrieve(image_url, os.path.join(root_dir, 'images', image_name))
+
+main()
